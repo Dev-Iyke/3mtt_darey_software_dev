30b90307590d41fc03c99f180aafb9701e038e24
Dev-Iyke <iyke.thompson01@gmail.com>
2025-04-13 16:49:49 +0100

    Built a simple arithmethic calculator

---
+++ module_1/Simple-Pocket-Calculator/main.js
@@ -0,0 +1,27 @@
+const output = document.getElementById('screen')
+console.log(output)
+
+ function inputValue(value){
+    if(output.textContent.length > 10){
+        output.textContent = 'Too many characters'
+    }else{
+        output.textContent += value
+    }
+    
+}
+function clearOutput(){
+    output.textContent = ''
+}
+function showResult(){
+    try{
+        const result = eval(output.textContent)
+        console.log(result)
+        output.textContent =  result.toFixed(5)
+    }
+    catch(error){
+        output.textContent = 'Syntax Error';
+        
+    }
+    
+    
+}
