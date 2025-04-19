afde677ac863658982cfb17d9d2eda6d923151ed
Dev-Iyke <iyke.thompson01@gmail.com>
2025-04-19 14:00:47 +0100

    Built a simple react counter app

---
+++ module_2/counter-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import CounterApp from './components/Counter';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Counter App</h1>
+      <p>Track how fast can you click on buttons</p>
+
+      <CounterApp />
+    </div>
+  );
+}
+
+export default App;
---
+++ module_2/counter-app/src/components/Counter.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+
+function CounterApp() {
+  const [count, setCount] = useState(0);
+  const countLimit = 10
+  useEffect(() => {
+    if(count === countLimit){
+      alert("You've reached the limit. well done!")
+      setCount(0)
+    }
+  }, [count])
+  return (
+    <div className="count-container">
+      <div className="count">
+        <h2>{count}</h2>
+      </div>
+      <div className="button-container">
+        <button
+          disabled={count <= 0} 
+          onClick={() => setCount(count - 1)}
+        >
+          - Decrease
+        </button>
+        <button
+          disabled={count === countLimit} 
+          onClick={() => setCount(count + 1)}
+        >
+          Increase +
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default CounterApp;
